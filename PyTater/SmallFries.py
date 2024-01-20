a04438683bc71ba0fb69f392bc5e8d4f458319d3
John <john@beyondvoip.net>
2024-01-19 18:33:43 -0800

    Add 3rd miner

--- PyTater/SmallFries.py
+++ PyTater/SmallFries.py
@@ -14,7 +14,8 @@ load_dotenv()
 
 miner_id_1 = os.getenv('miner_id_1')
 miner_id_2 = os.getenv('miner_id_2')
-all_miners = [miner_id_1, miner_id_2]
+miner_id_3 = os.getenv('miner_id_3')
+all_miners = [miner_id_1, miner_id_2, miner_id_3]
 
 
 
